b317c3edafbee4d366562e4777aa54d38d0eec5f
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2021-05-08 22:02:17 +0900

    bible_word_Proto

--- mybible_app/lib/screen/contents/bibleWord.dart
+++ mybible_app/lib/screen/contents/bibleWord.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class Word extends StatefulWidget {
@@ -6,14 +7,17 @@ class Word extends StatefulWidget {
 }
 
 class _WordState extends State<Word> {
+  
   @override
   Widget build(BuildContext context) {
+    Size size =MediaQuery.of(context).size;
+    final Controller =TextEditingController();
     return MaterialApp(
       theme: ThemeData(
           appBarTheme: AppBarTheme(
-              backgroundColor: Colors.white,
+              backgroundColor: Colors.blueAccent,
               iconTheme: IconThemeData(
-                  color: Colors.black
+                  color: Colors.white
               )
           )
       ),
@@ -21,14 +25,175 @@ class _WordState extends State<Word> {
         backgroundColor: Colors.white,
         appBar: AppBar(
           elevation: 0,
-          leading:
-          IconButton(onPressed: (){
+          leading: IconButton(
+              onPressed: (){
             Navigator.pop(context);
-          }, icon: Icon(Icons.arrow_back))
-          ,
-          title: Text("말씀",style: TextStyle(color: Colors.black),),
+          }, icon: Icon(Icons.arrow_back)),
+          title: Text("말씀",
+            style: TextStyle(color: Colors.white),),
         ),
-      ),
+        body: Column(
+          children: [
+            Container(
+              height: size.height*0.12,
+              child: Stack(
+                children: [
+                  Container(
+                    height: size.height*0.12-27,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft:Radius.circular(36),
+                          bottomRight:Radius.circular(36),
+                        )
+                    ),
+                  ),
+                 Positioned(
+                   bottom: 0,
+                   left: 0,
+                   right: 0,
+                   child:Container(
+                     margin: EdgeInsets.symmetric(horizontal: 10),
+                     height: 54,
+                     decoration: BoxDecoration(
+                       color: Colors.white,
+                       borderRadius: BorderRadius.circular(20),
+                       boxShadow: [
+                         BoxShadow(
+                           offset: Offset(0,4),
+                            blurRadius:50,
+                           color: Colors.lightBlueAccent
+                         )
+                       ]
+                     ),
+                     child: TextField(
+                       decoration: InputDecoration(
+                         hintText: "   Search",
+                         hintStyle: TextStyle(color: Colors.black),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                       ),
+                     ),
+                   ),)
+                ],
+              ),
+            ),
+           SizedBox(
+             height: 100,
+           ),
+           Flexible(
+             flex: 2,
+             child:  Row(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text(" 구약",
+                   style: TextStyle(
+                   fontSize: 30,
+                     fontWeight: FontWeight.bold
+                 ),)
+               ],
+             ),
+           ),
+            SizedBox(
+              height: 10,
+            ),
+            Flexible(
+              flex: 3,
+                child:ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 160.0,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.orange,
+                    )
+                  ],
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Flexible(
+              flex: 2,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(" 신약",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                    ),)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Flexible(
+              flex: 3,
+                child:ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 160.0,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.orange,
+                    )
+                  ],
+                ) ),
+
+      ]),
+    )
     );
+
   }
 }
+
+class CustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    double height = size.height;
+    double width = size.width;
+    path.lineTo(0, height - 30);
+    path.quadraticBezierTo(width /1, height, width, height - 50);
+    path.lineTo(width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
