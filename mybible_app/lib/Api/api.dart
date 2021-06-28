7218d585a4d8f6a8e7a63b9e651c886eeb51818f
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2021-06-28 23:06:06 +0900

    bible_done

--- mybible_app/lib/Api/api.dart
+++ mybible_app/lib/Api/api.dart
@@ -10,44 +10,90 @@ import 'package:html/dom_parsing.dart';
 
 //http://ibibles.net/quote.php?kor-mat/5:3-12
 
-class Api{
-  var url= Uri.parse("http://ibibles.net/quote.php?kor-mat/5:3-12") ;
+class Api {
+  var url = Uri.parse("http://ibibles.net/quote.php?kor-mat/5:3-12");
+
   var response;
-  Map<String,dynamic> result={};
+  int max_index = 22;
+  Map<String, dynamic> result = {};
+  List<String> lis = List<String>();
 
 
   //오늘의 말씀
-  Future<Map<String,dynamic>> getTodayWord() async {
-    response= await http.get(url).timeout(const Duration(seconds:3));
-    result= json.decode(response.body);
+  Future<Map<String, dynamic>> getTodayWord() async {
+    response = await http.get(url).timeout(const Duration(seconds: 3));
+    result = json.decode(response.body);
     return result;
   }
 
 
   //성경 말씀
-  Future<String> getBibleWord(String book_name,String starting_chapter,String starting_verse,String ending_verse) async{
-    String parses="http://ibibles.net/quote.php?kor-"+book_name+"/"+starting_chapter+":"+starting_verse+"-"+ending_verse;
-    var url= Uri.parse(parses);
+  Future<List<String>> getBibleWord(String book_name, String starting_chapter,
+      String starting_verse, String ending_verse) async {
+    String parses = "http://ibibles.net/quote.php?kor-" + book_name + "/" +
+        starting_chapter + ":" + starting_verse + "-" + ending_verse;
+    var url = Uri.parse(parses);
     var response = await http.get(url);
     var result = response.body;
     var docu = parse(response.body);
-    var view= docu.querySelectorAll("body");
+    var view = docu.querySelectorAll("body");
 
-    for(var e in view){
-      var result=e.text;
+    for (var e in view)
+      result = e.text;
 
+    int end = (int.parse(ending_verse));
+    int start = int.parse(starting_verse);
+
+    if (starting_verse == '0') {
+      for (int i = 1; i < 23; i++) {
+        if (i == end - 1) {
+          lis.add(result.substring(
+            result.indexOf((starting_chapter + ':' + (i).toString())),));
+          return lis;
+        }
+        lis.add(result.substring(
+            result.indexOf((starting_chapter + ':' + i.toString())),
+            result.indexOf(starting_chapter + ':' + (i + 1).toString())));
+      }
+      return lis;
     }
+    else {
+      if (starting_verse == ending_verse) {
+        int eindex = result.indexOf(starting_chapter + ':' + ending_verse);
+        lis.add(result.substring(
+            result.indexOf(starting_chapter + ':' + starting_verse, eindex)));
+        return lis;
+      }
+      for (int i = start; i < end; i++) {
+        if (i == end - 1) {
+          lis.add(result.substring(
+            result.indexOf((starting_chapter + ':' + (i).toString())),));
+          return lis;
+        }
+        lis.add(result.substring(
+            result.indexOf((starting_chapter + ':' + i.toString())),
+            result.indexOf(starting_chapter + ':' + (i + 1).toString())));
+      }
 
-
-    return result;
+      //print(lis);
+      return lis;
+    }
   }
 
-  void selecting(String text){
-    var document = parse(text);
+// void selecting(String book_name){
+//   switch(book_name){
+//     case "룻기":
+//       //4
+//       max_index=;break;
+//     case "오바댜":
+//       //1
+//       max_index=;break;
+//     case "빌립보서":
+//       //4
+//       max_index=;break;
+//     case "데살로니가후서":
+//       //3
+//       max_index=;break;
+//   }
 
-    // var elements= document.getElementsByTagName("br");
-    var elements= document.querySelectorAll("body > small");
-
-    print(elements);
-  }
 }
--- mybible_app/lib/screen/contents/bibleWord.dart
+++ mybible_app/lib/screen/contents/bibleWord.dart
@@ -1,16 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mybible_app/Api/api.dart';
 
+
+
+int selec_inedx=0;
+String book=null;
+List<List<int>> profile=[
+  [4,22,23,18,22],
+  [1,21],
+  [4,30,30,21,23],
+  [3,12,17,18]
+];
 class Word extends StatefulWidget {
   @override
   _WordState createState() => _WordState();
 }
 
 class _WordState extends State<Word> {
-  
   @override
   Widget build(BuildContext context) {
+    List<String> post_bible=['룻기','오바댜'];
+    List<String> past_bible=['빌립보서','데살로니가후서'];
     Size size =MediaQuery.of(context).size;
     final Controller =TextEditingController();
     return MaterialApp(
@@ -30,58 +42,62 @@ class _WordState extends State<Word> {
               onPressed: (){
             Navigator.pop(context);
           }, icon: Icon(Icons.arrow_back)),
+          actions: [
+            new IconButton(
+                onPressed: (){
+                  Navigator.of(context).push(_createRoute());
+                }, icon: Icon(Icons.search))
+          ],
           title: Text("말씀",
             style: TextStyle(color: Colors.white),),
         ),
         body: Column(
           children: [
             Container(
-              height: size.height*0.12,
+              height: size.height*0.1,
               child: Stack(
                 children: [
                   Container(
-                    height: size.height*0.12-27,
+                    height: size.height*0.09-32,
                     decoration: BoxDecoration(
                         color: Colors.grey,
                         borderRadius: BorderRadius.only(
-                          bottomLeft:Radius.circular(36),
-                          bottomRight:Radius.circular(36),
+                          bottomLeft:Radius.circular(26),
+                          bottomRight:Radius.circular(26),
                         )
                     ),
                   ),
-                 Positioned(
-                   bottom: 0,
-                   left: 0,
-                   right: 0,
-                   child:Container(
-                     margin: EdgeInsets.symmetric(horizontal: 10),
-                     height: 54,
-                     decoration: BoxDecoration(
-                       color: Colors.white,
-                       borderRadius: BorderRadius.circular(20),
-                       boxShadow: [
-                         BoxShadow(
-                           offset: Offset(0,4),
-                            blurRadius:50,
-                           color: Colors.grey
-                         )
-                       ]
-                     ),
-                     child: TextField(
-                       decoration: InputDecoration(
-                         hintText: "   Search",
-                         hintStyle: TextStyle(color: Colors.black),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                       ),
-                     ),
-                   ),)
+                 // Positioned(
+                 //   bottom: 0,
+                 //   left: 0,
+                 //   right: 0,
+                 //   child:Container(
+                 //     margin: EdgeInsets.symmetric(horizontal: 10),
+                 //     height: 54,
+                 //     decoration: BoxDecoration(
+                 //       color: Colors.white,
+                 //       borderRadius: BorderRadius.circular(20),
+                 //       boxShadow: [
+                 //         BoxShadow(
+                 //           offset: Offset(0,4),
+                 //            blurRadius:50,
+                 //           color: Colors.grey
+                 //         )
+                 //       ]
+                 //     ),
+                 //     child: TextField(
+                 //       decoration: InputDecoration(
+                 //         hintText: "   Search",
+                 //         hintStyle: TextStyle(color: Colors.black),
+                 //        enabledBorder: InputBorder.none,
+                 //        focusedBorder: InputBorder.none,
+                 //       ),
+                 //     ),
+                 //   ),)
                 ],
               ),
             ),
-           SizedBox(
-             height: 100,
-           ),
+
            Flexible(
              flex: 2,
              child:  Row(
@@ -95,50 +111,47 @@ class _WordState extends State<Word> {
                ],
              ),
            ),
+
             SizedBox(
               height: 10,
             ),
             Flexible(
               flex: 4,
-                child:FutureBuilder(
-                  future: Api().getBibleWord("","","",""),
-                  builder: (BuildContext contest,AsyncSnapshot snapshot){
-                    return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemCount: 10,
-                      itemBuilder: (BuildContext context, int index){
-                        return Container(
-                          width: 150,
-                          decoration: BoxDecoration(
+                child:ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: post_bible.length,
+                    itemBuilder: (BuildContext context, int index){
+                      return Container(
+                        width: 150,
+                        decoration: BoxDecoration(
                             image: DecorationImage(
                               image: AssetImage("image/bible.png"),
                             )
+                        ),
+                        child: InkWell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text("<${post_bible[index]}>",
+                                style:TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),),
+                            ],
                           ),
-                          child: InkWell(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Text("<${index}>",
-                                  style:TextStyle(
-                                      color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),),
-                              ],
-                            ),
-                            onTap: (){
-                              Navigator.push(context,
-                                  MaterialPageRoute(
-                                      builder: (context)=> Bible_page(context,index)));
-                            },
-                          ),
-                        );
-                      }
-                    );
-                  },
+                          onTap: (){
+                            Navigator.push(context,
+                                MaterialPageRoute(
+                                    builder: (context)=> Bible_page(context,post_bible[index])));
+                          },
+                        ),
+                      );
+                    }
                 )),
+
             SizedBox(
               height: 10,
             ),
@@ -155,58 +168,100 @@ class _WordState extends State<Word> {
                 ],
               ),
             ),
+
             SizedBox(
               height: 10,
             ),
             Flexible(
                 flex: 4,
-                child:FutureBuilder(
-                  future: Api().getBibleWord("","","",""),
-                  builder: (BuildContext contest,AsyncSnapshot snapshot){
-                    return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: 10,
-                        itemBuilder: (BuildContext context, int index){
-                          return Container(
-                            width: 150,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("image/bible.png"),
-                                )
-                            ),
-                            child: InkWell(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text("<${index}>",
-                                    style:TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),),
-                                ],
-                              ),
-                              onTap: (){
-                                Navigator.push(context,
-                                    MaterialPageRoute(
-                                        builder: (context)=> Bible_page(context,index)));
-                              },
-                            ),
-                          );
-                        }
-                    );
-                  },
-                )),
-
+                child:ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: past_bible.length,
+                    itemBuilder: (BuildContext context, int index){
+                      return Container(
+                        width: 150,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("image/bible.png"),
+                            )
+                        ),
+                        child: InkWell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text("<${past_bible[index]}>",
+                                style:TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),),
+                            ],
+                          ),
+                          onTap: (){
+                            Navigator.push(context,
+                                MaterialPageRoute(
+                                    builder: (context)=> Bible_page(context,past_bible[index])));
+                          },
+                        ),
+                      );
+                    }
+                )
+            ),
       ]),
     )
     );
 
   }
+  Route _createRoute(){
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => Serch_page(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
 
-  Widget Bible_page(BuildContext context, int name){
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+
+  Widget Serch_page(){
+    return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.grey,
+              iconTheme: IconThemeData(
+                  color: Colors.white
+              )
+          )
+      ),
+      home: Scaffold(
+        appBar:AppBar(
+        elevation: 0,
+        leading: IconButton(
+            onPressed: (){
+              Navigator.pop(context);
+            }, icon: Icon(Icons.arrow_back)),
+        title: Text("검색",
+          style: TextStyle(color: Colors.white),),
+      ),
+        body: Center(
+          child: Text("dsa"),
+        ),
+      ),
+    );
+  }
+
+  Widget Bible_page(BuildContext context, String name){
+    Selec(name);
     return MaterialApp(
       theme: ThemeData(
           appBarTheme: AppBarTheme(
@@ -224,17 +279,96 @@ class _WordState extends State<Word> {
               onPressed: (){
                 Navigator.pop(context);
               }, icon: Icon(Icons.arrow_back)),
-          title: Text("${name}",
+          title: Text(name,
             style: TextStyle(color: Colors.white),),
         ),
-        body: Center(
-          child: Text("das"),
+        body:ListView.builder(
+            itemCount: profile[selec_inedx][0],
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text(name+(index+1).toString()+"장"),
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.add
+                  ),
+                  onPressed: (){
+                    Navigator.push(context,
+                        MaterialPageRoute(
+                            builder: (context)=> Seconde_page(name,(index+1).toString())));
+                  },
+                ),
+              );
+            })
+      ),
+    );
+  }
+
+  void Selec(String name){
+    String result;
+    switch(name){
+      case "룻기":
+        selec_inedx=0;
+        book="ruth";break;
+      case "오바댜":
+        selec_inedx=1;
+        book="obad";break;
+      case "빌립보서":
+        selec_inedx=2;
+        book="phi";break;
+      case "데살로니가후서":
+        selec_inedx=3;
+        book="2th";break;
+    }
+
+  }
+
+
+
+  Widget Seconde_page(name,bible_index){
+    return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.grey,
+              iconTheme: IconThemeData(
+                  color: Colors.white
+              )
+          )
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          leading: IconButton(
+              onPressed: (){
+                Navigator.pop(context);
+              }, icon: Icon(Icons.arrow_back)),
+          title: Text(name+bible_index+"장",
+            style: TextStyle(color: Colors.white),),
+        ),
+        body: FutureBuilder(
+          future: Api().getBibleWord(book, bible_index, "0",profile[selec_inedx][int.parse(bible_index)].toString()),
+          builder: (BuildContext contest,AsyncSnapshot snapshot){
+
+           if(snapshot.hasData){
+             return ListView.builder(
+                 itemCount: profile[selec_inedx][int.parse(bible_index)]-1,
+                 itemBuilder: (contest,index){
+                   return ListTile(
+                     title: Text(snapshot.data[index].toString()),
+                   );
+                 });
+           }
+           else{
+             return Center(child: CupertinoActivityIndicator());
+           }
+          } ,
         ),
       ),
     );
   }
 
 }
+
+
 class CustomShape extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
